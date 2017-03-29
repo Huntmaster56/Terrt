93e058931de9e7d7c7ac69e009c239ef338dcd10
joshua.livingst <Huntmaster56>
2017-03-29 10:29:12 -0700

    Done

---
+++ Assets/Scripts/ResetOnFall.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ResetOnFall : MonoBehaviour {
+
+
+    private Vector3 StartPOS;
+    public float Falling;
+    void Start()
+    {
+        StartPOS = transform.position;
+    }
+    // Update is called once per frame
+    void Update ()
+    {
+            if (transform.position.y < (-10))
+            {
+            transform.position = StartPOS;//Falling;
+            }
+    }
+}
---
+++ Assets/Scripts/ReturnToSender.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ReturnToSender : MonoBehaviour
+{
+
+    private Vector3 StartPOS;
+    private Rigidbody rbody;
+
+    void Start()
+    {
+        StartPOS = transform.position;
+        rbody = GetComponent<Rigidbody>();
+    }
+
+
+    void OnTriggerEnter(Collider other)
+    {
+        if(other.CompareTag("KillTrigger"))
+        {
+            transform.position = StartPOS;
+            if(rbody)
+            {
+                rbody.velocity = Vector3.zero;
+            }
+        }
+    }
+
+
+}
